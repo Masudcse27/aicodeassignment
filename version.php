b875aeb6a45b5151fea639147cecf8333cf2cac1
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-07-02 16:54:33 +0600

    Initialize project and create initial database schema

---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+/**
+ * Version details for mod_aicodeassignment
+ * @package    mod_aicodeassignment
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_aicodeassignment';  
+$plugin->version   = 2025070200;              
+$plugin->requires  = 2022041900;              
+$plugin->maturity  = MATURITY_ALPHA;          
+$plugin->release   = '1.0.0';                 
